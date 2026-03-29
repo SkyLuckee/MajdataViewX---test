7bd5d8fdaabc28c88c35f4e1d659022fa5a74414
SkyLuckee <locnd07@gmail.com>
2026-03-30 00:16:01 +0800

    refactor: remove unused bpm code and comments in AudioTimeProvider

--- Assets/Scripts/AudioTimeProvider.cs
+++ Assets/Scripts/AudioTimeProvider.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using UnityEngine;
-// using System.Collections.Generic;
-// using System.Linq;
-// using MajSimai;
 
 public class AudioTimeProvider : MonoBehaviour
 {
@@ -35,24 +32,8 @@ public class AudioTimeProvider : MonoBehaviour
         return _audioTime / 16.6667f;
     }
 
-    // optional field to keep the extracted BPM if you want it later
-    // public float FirstBpm { get; private set; } = -1f;
-
-    // Old 3-argument call compatibility
-    // public void SetStartTime(long _ticks, float _offset, float _speed)
-    // {
-    //     SetStartTime(_ticks, _offset, _speed, (IEnumerable<SimaiTimingPoint>)null, false);
-    // }
-
-    // Old 4-argument call where the 4th argument is the record flag
-    // public void SetStartTime(long _ticks, float _offset, float _speed, bool _isRecord)
-    // {
-    //     SetStartTime(_ticks, _offset, _speed, (IEnumerable<SimaiTimingPoint>)null, _isRecord);
-    // }
-
-    public void SetStartTime(long _ticks, float _offset, float _speed, bool _isRecord = false) //IEnumerable<SimaiTimingPoint> timings
+    public void SetStartTime(long _ticks, float _offset, float _speed, bool _isRecord = false)
     {
-        // float firstBpm = timings?.FirstOrDefault()?.Bpm ?? -1f;
         ticks = _ticks;
         offset = _offset;
         AudioTime = offset;
@@ -61,7 +42,7 @@ public class AudioTimeProvider : MonoBehaviour
         isRecord = _isRecord;
         if (_isRecord)
         {
-            startTime = Time.time + 5; //+ 60*4 / firstBpm;
+            startTime = Time.time + 5;
             Time.timeScale = _speed;
             Time.captureFramerate = 60;
         }
